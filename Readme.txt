b529f1fa2b672cdd674b34e98948157fdd732995
mauroisoardo <mauro.isoardo@hpe.com>
2016-01-22 14:43:06 +0100

    My first commit

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+Edit the file "Stackato.yml"
+Change: "app-dir" (you own app path on your computer).
+Change: "name" (Name of Your App / Subdomain)
+
+For a demo, edit the page "pages/home.php"
+
+Then... HELION PUSH
---
+++ pages/home.php
@@ -0,0 +1,11 @@
+<?php
+echo '<section id="cta" class="wrapper style3">
+           
+<h2>Welcome to the Virtual Hybrid-IT Maturity Program for Andy</h2>
+
+<ul class="actions">
+            <li><a href="#" class="button big">'.getenv("VCAP_APP_PORT").'</a></li>
+</ul>
+</section>';
+?>
+     
